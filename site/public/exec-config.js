2babf987731ea757f9303bb3111c528c6693c678
Sahil Ambegaonkar <sahil@Sahils-MacBook-Pro.local>
2026-09-17 23:41:14 -0400

    Connect volunteer hours approval service

--- site/public/exec-config.js
+++ site/public/exec-config.js
@@ -1,3 +1,3 @@
 window.NAVIO_EXEC_CONFIG = Object.freeze({
-  volunteerHoursEndpoint: "",
+  volunteerHoursEndpoint: "https://script.google.com/macros/s/AKfycbzNxT1LART-jNr8xgK2hTdR3MGB5S6em9AbAbHpkLTMCUeZ2k6q4KJnuN07vTcMuO37/exec",
 });
